0757e19c8ec0d99d353e98de11576903fd21e2de
Anshika Kapoor <ak7269@nyu.edu>
2020-02-06 20:18:29 +0000

    hand in lab1

---
+++ lab1/fromscratch/first3/first3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int  main(int argc, char** argv)
+{
+     char *a=argv[1];
+     char *b=argv[2];
+
+   if (argc!=3) 
+   {
+     printf("Not 2 arguments!");
+     return 0;
+  }
+
+    
+
+    int length_a=strlen(a);
+    int length_b=strlen(b);
+
+	if(length_a<3)
+	{
+		if(length_b<3){
+			printf("Strings are too short!");
+			return 0;
+		}
+		
+	}
+    for(int i=0;i<3;i++)
+    {
+	  printf("%c",a[i]);
+    }
+
+    int j=0;
+
+    while(j<3)
+    {
+    printf("%c",b[j]);
+    j++;
+    }
+	return 0;
+
+}
